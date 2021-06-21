c44b940768d5fa05e6bf3e3ec346299c96efd570
yourusername <jadenitripp@gmail.com>
2021-06-21 11:41:23 -0600

    made changes to seed file

--- server/controllers/requestsController.js
+++ server/controllers/requestsController.js
@@ -2,7 +2,8 @@ module.exports={
     createRequest: (req, res) => {
        const db = req.app.get('db')
        const {sender_id, receiver_id} = req.body
-       db.request.create_request(sender_id, receiver_id).then(results=> res.status(200).send(results))
+       db.request.create_request(sender_id, receiver_id)
+       .then(results=> res.status(200).send(results))
        .catch(err=>console.log(err))
     },
     deleteRequest: (req, res) => {
