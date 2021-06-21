87666f99e2c9a912302db4959172830ff13d755e
Joshua Leavitt <joshuadreyleavitt@gmail.com>
2021-06-21 15:32:44 -0600

    Fixed problem

--- src/components/RegisterSurvey/RegisterSurveyPage.js
+++ src/components/RegisterSurvey/RegisterSurveyPage.js
@@ -36,44 +36,44 @@ const useStyles = createUseStyles({
 })
 
 
-const RegisterSurveyPage = ( {history, setUser} ) => {
-  const {registerForm} = useStyles()
+const RegisterSurveyPage = ({ history, setUser }) => {
+  const { registerForm } = useStyles()
 
   const finishRegister = body => {
     axios.put('/auth/finishregister', body)
-    .then(results=> {
-      toast.success('Account Creation completed!')
-      setUser(results.data)
-      history.push('/swipingpage')
-    })
-    .catch(err=> toast.error(err.response.data))
+      .then(results => {
+        toast.success('Account Creation completed!')
+        setUser(results.data)
+        history.push('/swipingpage')
+      })
+      .catch(err => toast.error(err.response.data))
   }
   return (
-   <Formik
-        initialValues={{
-            first: '',
-            last: '',
-            age: '',
-            gender: '',
-            rel_type: '',
-            sexual_or: ''
-        }}
-        onSubmit={( {first, last, age, gender, rel_type, sexual_or} )=> {
-            finishRegister({ first, last, age, gender, rel_type, sexual_or })
-        }}>
+    <Formik
+      initialValues={{
+        first: '',
+        last: '',
+        age: '',
+        gender: '',
+        rel_type: '',
+        sexual_or: ''
+      }}
+      onSubmit={({ first, last, age, gender, rel_type, sexual_or }) => {
+        finishRegister({ first, last, age, gender, rel_type, sexual_or })
+      }}>
 
       {(values, isSubmitting) => (
         <Form className={registerForm}>
 
 
-          {/* <Button
+          <Button
             disabled={isSubmitting}
             type="submit"
             variant="contained"
             color="secondary"
-          > */}
-          <a href='https://video.search.yahoo.com/search/video?fr=mcafee&ei=UTF-8&p=never+gonna+give+you+up&type=E211US1485G0#id=0&vid=75170fc230cd88f32e475ff4087f81d9&action=click'>Submit</a>
-          {/* </Button> */}
+          >
+            Submit
+          </Button>
         </Form>
       )}
     </Formik>
